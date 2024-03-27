88d2a6385550566080d2dae45a6d55edbd8fb658
krishnavyas99 <84672834+krishnavyas99@users.noreply.github.com>
2024-03-27 18:19:29 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -37,12 +37,12 @@ resource "aws_elb" "bar" {
   preferred_backup_window = "07:00-09:00"
 }
 
-/*resource "aws_instance" "one" {
+resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
   ami           = "ami-006be9ab6a140de6e"
   instance_type = "t2.micro"
   tags = {
     Name = "instance-${each.key}"
   }
-}*/
+}
 
