6c9d5ce5165c56bbb5379fab6c458991c7bece3d
krishnavyas99 <84672834+krishnavyas99@users.noreply.github.com>
2024-03-27 11:09:30 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
+  name               = "kvl-elb"
   availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
 
   listener {
@@ -31,7 +31,7 @@ resource "aws_elb" "bar" {
   engine_version          = "5.7.mysql_aurora.2.11.2"
   availability_zones      = ["ap-southeast-1a", "ap-southeast-1b"]
   database_name           = "mydb"
-  master_username         = "raham"
+  master_username         = "Krishna"
   master_password         = "Rahamshaik#444555"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
