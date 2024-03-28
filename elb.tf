9a9bc5cf57a4c0387fa030e40a6be88d69c5faa8
krishnavyas99 <84672834+krishnavyas99@users.noreply.github.com>
2024-03-28 11:27:28 +0530

    NeweUpdate elb.tf

--- elb.tf
+++ elb.tf
@@ -39,7 +39,7 @@ resource "aws_elb" "bar" {
 
 resource "aws_instance" "kkvl" {
   for_each = toset(["k", "r", "i"])
-  ami           = "ami-006be9ab6a140de6e"
+  ami           = "ami-06018068a18569ff2"
   instance_type = "t2.micro"
   tags = {
     Name = "instance-${each.key}"
